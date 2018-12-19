49a39e28979ace4483e7ee086db4124366e32c17
JakubKolodziej95 <37178990+JakubKolodziej95@users.noreply.github.com>
2018-12-19 21:08:09 +0000

    failed attempt at mvvm

---
+++ CourierApp/CourierApp/Models/ProfileInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CourierApp
+{
+    public class ProfileInfo : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string name, id;
+        public string userName
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged("userName"); }
+        }
+
+        public string userID
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("userID"); }
+        }
+
+        private void OnPropertyChanged (string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
